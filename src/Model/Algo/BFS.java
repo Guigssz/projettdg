3413f76638976747c915510d7e2ea260e690187f
Janpeerbolte <jan@peerbolte.org>
2025-11-21 15:00:45 +0100

    Debut Bfs

--- src/Model/Algo/BFS.java
+++ src/Model/Algo/BFS.java
@@ -1,7 +1,30 @@
 package Model.Algo;
 
+
+import Model.Graphe.*;
+import Model.ResultatCommun.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
 public class BFS {
 
+
+    public static Itineraire bfs(Graphe g , Sommet depart, Sommet arrivee) {
+
+
+        if (depart == arrivee) return new Itineraire(depart, arrivee, new ArrayList<>(), 0 );
+
+        Map< Sommet , Liaison> predecesseur = new HashMap<>();
+
+        predecesseur.put(depart, null); é""
+
+        // file d'arcs a explorer en attente fifo
+        LinkedList<Arc> file = new LinkedList<>();
+        return Itineraire;
+    }
 
 
 
